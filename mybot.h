06180d6450d9cefa8653ea2b826f04f184b368de
Sara Silva <112500222+Saransilv@users.noreply.github.com>
2023-11-17 13:18:22 -0300

    Add files via upload

---
+++ mybot.h
@@ -0,0 +1,23 @@
+#define PINO_MOTOR_1_A 2 
+#define PINO_MOTOR_1_B 3
+#define PINO_MOTOR_2_A 12 
+#define PINO_MOTOR_2_B 13
+#define PINO_SENSOR_ESQUERDA 8
+#define PINO_SENSOR_DIREITA 
+
+
+int motor_1_frente();
+int motor_1_tras();
+int motor_1_parar();
+int motor_2_frente();
+int motor_2_tras();
+int motor_2_parar();
+
+
+int frente();
+int tras();
+int esquerda();
+int direita();
+int parar();
+
+
